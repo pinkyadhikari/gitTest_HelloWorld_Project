6ae5bff2b8bb5323cf7b3c1d8f6f69f60ade47a6
pink13 <pinky.adhikari1@marist.edu>
2020-04-07 13:39:55 -0500

    git hub url comment added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,12 @@ public class Main {
 	// write your code here
         System.out.println("hello world ! from github.com..");
         System.out.print("Sandesh Lamsal");
+
+
+        int[] intArray ={100,200,300};
+        for(int a :intArray) {
+            System.out.println(a);
+        }
 	
     }
 }
